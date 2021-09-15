94c024c65fbc5f95cfc48bdef5acad40df146f0a
Jamese Victoria <jamesevictoria@Jameses-MacBook-Pro.local>
2021-09-15 15:57:19 -0700

    po

--- guess/index.js
+++ guess/index.js
@@ -1,1 +1,22 @@
-alert("u are a baby")
+var form = document.getElementById("form")
+var input = document.getElementById("input")
+var number = Math.random()
+number = 100 * number
+number = Math.ceil(number)
+
+
+function submit(e) {
+    e.preventDefault()
+    var guess = input.value
+    guess = parseInt(guess)
+    if (guess < number) {
+        alert("too small")
+    } else if (guess > number) {
+        alert("too big")
+    } else {
+        alert("you win!!!!")
+    }
+    input.value = "";
+    return false
+}
+form.onsubmit = submit
