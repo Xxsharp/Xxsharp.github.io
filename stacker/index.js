e7a0bc8bf3ef8a08798c068e1b4b065f6632a714
Jamese Victoria <jamesevictoria@Jameses-MacBook-Pro.local>
2021-10-26 18:30:21 -0700

    h

--- stacker/index.js
+++ stacker/index.js
@@ -1,16 +1,15 @@
 function main() {
     clone("box")
     clone("row")
-    selectBox(1, 1)
-    selectBox(2, 2)
-    selectBox(3, 3)
-    selectBox(4, 4)
-    selectBox(5, 5)
+    var button = document.getElementById("button")
+    button.onclick = click
+    var julius = document.getElementById("julius")
+    julius.onclick = juliuss
 }
 
 function clone(className) {
     var element = document.getElementsByClassName(className)[0]
-    for (var i = 1; i <= 10; i++) {
+    for (var i = 1; i < 10; i++) {
         var clone = element.cloneNode(true)
         var parent = element.parentElement
         parent.appendChild(clone)
@@ -21,5 +20,43 @@ function selectBox(rowNumber, columnNumber) {
     var row = document.getElementsByClassName("row")[rowNumber]
     var box = row.getElementsByClassName("box")[columnNumber]
     box.classList.add("selected")
+}
+
+function clearRow(rowNumber) {
+    var row = document.getElementsByClassName("row")[rowNumber]
+    for (var i = 0; i < 10; i++) {
+        row.children[i].classList.remove("selected")
+    }
+}
+
+function selectBoxes(rowNumber, columnNumber, numBoxes) {
+    clearRow(rowNumber)
+    for (var i = 0; i < numBoxes; i++) {
+        selectBox(rowNumber, columnNumber + i)
+    }
+}
+var rowNumber = 0
+var columnNumber = 0
+var isForward = true
+
+function click() {
+    selectBoxes(rowNumber, columnNumber, 1)
+    if (isForward) {
+        columnNumber++
+        if (columnNumber === 9) {
+            isForward = false
+        }
+    } else {
+        columnNumber--
+        if (columnNumber === 0) {
+            isForward = true
+        }
+    }
+    setTimeout(click, 174)
+}
+
+function juliuss() {
+    rowNumber = (rowNumber + 1) % 10
+
 }
 document.addEventListener("DOMContentLoaded", main)
